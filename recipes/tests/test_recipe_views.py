46e83d09a0833ac279e26cf885742d2c1f1922e3
Thiago Santos <playsd395@gmail.com>
2024-02-28 17:08:06 -0300

    Extraindo métodos para reutilização

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,4 +1,4 @@
-from tests.test_recipe_base import RecipeTestBase
+from recipes.tests.test_recipe_base import RecipeTestBase
 from django.urls import reverse, resolve
 from recipes import views
 
@@ -19,7 +19,6 @@ class RecipeViewsTest(RecipeTestBase):
 
 
     def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
-        Recipe.objects.get(pk=1).delete()
         response = self.client.get(reverse('recipes:home'))
         self.assertIn(
             'No recipes found',
@@ -27,10 +26,14 @@ class RecipeViewsTest(RecipeTestBase):
             ) 
         
     def test_recipe_home_template_loads_recipes(self):
+        self.make_recipe(author_data={
+            'first_name': 'Thiago'
+        })
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         self.assertIn('Recipe Title', content)
         self.assertIn('10 Minutos', content)
+        self.assertIn('Thiago', content)
 
         ...
 
