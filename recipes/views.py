46a08c9cfe4866b51c6115e60c70ebe3f7807741
Thiago Santos <playsd395@gmail.com>
2024-03-22 20:52:30 -0300

    Paginação padrão do django

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render, Http404
 from django.db.models import Q
 from recipes.models import Recipe
+from django.core.paginator import Paginator
 
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
-    ).order_by('-id')
+    ).order_by('id')
+
+    current_page = request.GET.get('page', 1)
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
 
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
     })
 
 
