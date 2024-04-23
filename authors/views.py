54fb4ff3e79bca76c50cad72e61459b3d3431f22
Thiago Santos <playsd395@gmail.com>
2024-04-22 23:54:45 -0300

    Testando forms authors

--- authors/views.py
+++ authors/views.py
@@ -1,30 +1,27 @@
-from django.shortcuts import render, redirect
-from authors.forms import RegisterForm
+from django.contrib import messages
 from django.http import Http404
+from django.shortcuts import redirect, render
 
-def register_view(request): 
-    #request.session['number'] = request.session.get('number') or 1
-    #request.session['number'] += 1
+from .forms import RegisterForm
 
+def register_view(request):
     register_form_data = request.session.get('register_form_data', None)
-
-    form = RegisterForm()
-
-    #print(form)
-
+    form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
-        'form': form
+        'form': form,
     })
 
 def register_create(request):
     if not request.POST:
         raise Http404()
-    
     POST = request.POST
     request.session['register_form_data'] = POST
-
     form = RegisterForm(POST)
 
-    #print(POST)
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Your user is created, please log in.')
+
+        del(request.session['register_form_data'])
 
     return redirect('authors:register')
