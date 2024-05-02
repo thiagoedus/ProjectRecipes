3fdb4ee82bbc641ca059d20f97f3e83e339811f5
Thiago Santos <playsd395@gmail.com>
2024-05-02 14:30:36 -0300

    Configuração Selenium e primeiros testes

---
+++ tests/functional_tests/test_recipe_home_page.py
@@ -0,0 +1,25 @@
+import time
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from utils.browser import make_chrome_browser
+from selenium.webdriver.common.by import By
+
+class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
+
+    def setUp(self) -> None:
+        self.browser = make_chrome_browser()
+        return super().setUp()
+    
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
+    def sleep(self, seconds=5):
+        time.sleep(seconds)
+
+class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+    
+    def test_recipe_home_page_without_recipes_not_found_messages(self):
+        self.browser.get(self.live_server_url)
+        body = self.browser.find_element(By.TAG_NAME, 'body')
+        self.assertIn('No recipes found here 🥲', body.text)
---
+++ utils/browser.py
@@ -0,0 +1,30 @@
+import os
+
+from selenium import webdriver
+from pathlib import Path
+from time import sleep
+from selenium.webdriver.chrome.service import Service
+
+ROOT_PATH = Path(__file__).parent.parent
+CHROMEDRIVER_NAME = 'chromedriver.exe'
+CHROMEDRIVER_PATH = ROOT_PATH / 'bin' / CHROMEDRIVER_NAME
+
+
+#  --headless - O headless não mostra o navegador abrindo
+def make_chrome_browser(*options):
+    chrome_options = webdriver.ChromeOptions()
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
+    if os.environ.get('SELENIUM_HEADLESS') == 1:
+        chrome_options.add_argument('--headless')
+
+    chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
+    return webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+if __name__ == '__main__':
+    browser = make_chrome_browser('--headless')
+    browser.get('http://www.udemy.com.br')
+    sleep(5)
+    browser.quit()
