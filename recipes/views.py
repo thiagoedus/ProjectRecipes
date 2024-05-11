84e2401561123e2fb8e6e2097a5a601e940551b0
Thiago Santos <playsd395@gmail.com>
2024-05-11 19:52:56 -0300

    Listando receitas na home com a classe ListView

--- recipes/views.py
+++ recipes/views.py
@@ -4,9 +4,31 @@ from recipes.models import Recipe
 from utils.pagination import make_pagination
 import os
 from django.contrib import messages
+from django.views.generic.list import ListView
 
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
+
+
+class RecipeListViewBase(ListView):
+    model = Recipe
+    context_object_name = 'recipes'
+    paginate_by = None
+    ordering = ['-id']
+    template_name = 'recipes/pages/home.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(is_published=True)
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+        page_obj, pagination_range = make_pagination(
+            self.request, context_data.get('recipes'), PER_PAGE)
+        context_data.update(
+            {'recipes': page_obj, 'pagination_range': pagination_range})
+        return context_data
 
 
 def home(request):
@@ -32,7 +54,6 @@ def category(request, category_id):
 
     page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
-
     return render(request, 'recipes/pages/category.html', context={
         'recipes': page_obj,
         'pagination_range': pagination_range,
@@ -48,18 +69,19 @@ def recipe(request, id):
         'is_detail_page': True,
     })
 
+
 def search(request):
     search_therm = request.GET.get('q', '').strip()
 
     if not search_therm:
         raise Http404()
-    
+
     recipes = Recipe.objects.filter(
         Q(
-            Q(title__icontains = search_therm) | 
-            Q(description__icontains = search_therm),
+            Q(title__icontains=search_therm) |
+            Q(description__icontains=search_therm),
         ),
-        is_published = True
+        is_published=True
     ).order_by('-id')
 
     page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
@@ -67,7 +89,7 @@ def search(request):
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{ search_therm }" |',
         'search_term': search_therm,
-        'recipes': page_obj, 
+        'recipes': page_obj,
         'pagination_range': pagination_range,
         'additional_url_query': f'&q={search_therm}'
     })
