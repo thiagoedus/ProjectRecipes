6bb9cdd90e85cc4f37de121a2f4adae9cb2a1b6a
Thiago Santos <playsd395@gmail.com>
2024-06-25 16:31:26 -0300

    Otimizando consultas de profiles

--- recipes/views.py
+++ recipes/views.py
@@ -38,7 +38,7 @@ class RecipeListViewBase(ListView):
         qs = qs.filter(is_published=True)
 
         qs = qs.select_related('author', 'category')
-        qs = qs.prefetch_related('tags')
+        qs = qs.prefetch_related('tags', 'author__profile')
 
         return qs
 
