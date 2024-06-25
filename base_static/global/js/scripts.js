02be99c7ab5e4ed9062a7046c6145d084acd15d9
Thiago Santos <playsd395@gmail.com>
2024-06-25 11:29:52 -0300

    exibindo e ocultando menu com js

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -1,4 +1,4 @@
-function my_scope() {
+(() => {
     const forms = document.querySelectorAll('.form-delete');
 
     for(const form of forms) {
@@ -12,7 +12,34 @@ function my_scope() {
             }
         })
     }
-}
+})();
 
+(() => {
+    const buttonCloseMenu = document.querySelector('.button-close-menu');
+    const buttonShowMenu = document.querySelector('.button-show-menu');
+    const menuContainer = document.querySelector('.menu-container');
 
-my_scope()
+    const buttonShowMenuVisibleClass = 'button-show-menu-visible';
+    const menuHiddenClass = 'menu-hidden';
+
+    const closeMenu = () => {
+        buttonShowMenu.classList.add(buttonShowMenuVisibleClass)
+        menuContainer.classList.add(menuHiddenClass)
+    }
+
+    const showMenu = () => {
+        buttonShowMenu.classList.remove(buttonShowMenuVisibleClass)
+        menuContainer.classList.remove(menuHiddenClass)
+    }
+
+    if (buttonCloseMenu) {
+        buttonCloseMenu.removeEventListener('click', closeMenu)
+        buttonCloseMenu.addEventListener('click', closeMenu)
+    }
+
+    if (buttonShowMenu) {
+        buttonShowMenu.removeEventListener('click', showMenu)
+        buttonShowMenu.addEventListener('click', showMenu)
+    }
+
+})();
