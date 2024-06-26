728410737a615779773734536cff0426ad177e33
Thiago Santos <playsd395@gmail.com>
2024-06-26 00:24:24 -0300

    Redimensionando imagens com pillow

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,4 @@
+import os
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
@@ -8,6 +9,8 @@ from django.db.models.functions import Concat
 from django.core.exceptions import ValidationError
 from tag.models import Tag
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+from PIL import Image
 
 
 class RecipeManager(models.Manager):
@@ -56,12 +59,38 @@ class Recipe(models.Model):
 
     def get_absolute_url(self):
         return reverse('recipes:recipe', args=(self.id,))
+    
+    def resize_image(image, new_width=800):
+        image_full_path = os.path.join(settings.MEDIA_ROOT, image.name)
+        image_pillow = Image.open(image_full_path)
+        original_width, original_height = image_pillow.size
+
+        if original_width <= new_width:
+            image_pillow.close()
+            return
+        
+        new_height = round((new_width * original_height) / original_width)
+        new_image = image_pillow.resize((new_width, new_height), Image.LANCZOS)
+
+        new_image.save(
+            image_full_path,
+            optimize=True,
+            quality=50,
+        )
 
     def save(self, *args, **kwargs):
         if not self.slug:
             slug = f'{slugify(self.title)}'
             self.slug = slug
-        return super().save(*args, **kwargs)
+
+        saved = super().save(*args, **kwargs)
+
+        if self.cover:
+            self.resize_image(image=self.cover, new_width=800)
+
+        return saved
+    
+        
 
     def clean(self, *args, **kwargs):
         error_messages = defaultdict(list)
---
+++ recipes/signals.py
@@ -0,0 +1,30 @@
+import os
+from django.db.models.signals import pre_delete, pre_save
+from django.dispatch import receiver
+
+from .models import Recipe
+
+def delete_cover(instance):
+    try:
+        os.remove(instance.cover.path)
+    except(ValueError, FileNotFoundError):
+        ...
+
+
+@receiver(pre_delete, sender=Recipe)
+def recipe_cover_delete(sender, instance, created, *args, **kwargs):
+    old_instance = Recipe.objects.filter(pk=instance.pk).first()
+    delete_cover(old_instance)
+
+
+@receiver(pre_save, sender=Recipe)
+def recipe_cover_update(sender, instance, created, *args, **kwargs):
+    old_instance = Recipe.objects.filter(pk=instance.pk).first()
+    is_new_cover = old_instance.cover != instance.cover
+
+    if not old_instance:
+        return
+
+    if is_new_cover:
+        delete_cover(old_instance)
+    print(old_instance.cover)
