ef49958e7dd1f7866a00bab1615134e88b3098bc
Thiago Santos <playsd395@gmail.com>
2024-06-26 11:21:09 -0300

    Serializer para list e detail com rest framework

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,9 +1,29 @@
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from ..models import Recipe
+from .serializers import RecipeSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 @api_view(http_method_names=['get', 'post'])
 def recipe_api_list(request):
-    return Response({
-        'name': 'blablabla'
-    })
+    recipes = Recipe.objects.get_published()[:10]
+    serializer = RecipeSerializer(instance=recipes, many=True)
+    return Response(serializer.data)
+
+@api_view(http_method_names=['get', 'post'])
+def recipe_api_detail(request, pk):
+    recipe = get_object_or_404(Recipe.objects.get_published(), pk=pk)
+    serializer = RecipeSerializer(instance=recipe, many=False)
+    return Response(serializer.data)
+
+    # recipe = Recipe.objects.get_published().filter(pk=id).first()
+
+    # if recipe:
+    #     serializer = RecipeSerializer(instance=recipe, many=False)
+    #     return Response(serializer.data)
+    # else:
+    #     return Response({
+    #         "detail": "ERRO"
+    #     }, status=status.HTTP_418_IM_A_TEAPOT)
